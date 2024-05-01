060d18d4cf29de88a32a47ad25ae8a1ff3a46e42
Smolev Denis <smolev@me.com>
2024-05-01 17:33:26 +0300

    og_meta_data_tags_better

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,7 @@
 module ApplicationHelper
-
+	def og_meta_data_tags_better
+      og_meta_data.map do |property, content|
+        tag('meta', property: property, content: strip_tags(content)) unless property.nil? || content.nil?
+      end.join("\n")
+    end
 end
