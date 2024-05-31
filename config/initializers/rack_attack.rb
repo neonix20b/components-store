b7d91432adb3c4f809d85482574fca19a293bc80
Smolev Denis <smolev@me.com>
2024-05-31 23:25:25 +0300

    ban

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -1,5 +1,7 @@
 Rack::Attack.blocklist_ip("65.108.2.0/16")
 Rack::Attack.blocklist_ip("23.22.35.0/16")
-Rack::Attack.throttle("requests by ip", limit: 5, period: 2) do |request|
-  request.ip
-end
+Rack::Attack.blocklist_ip("3.224.220.0/16")
+
+# Rack::Attack.throttle("requests by ip", limit: 5, period: 2) do |request|
+#   request.ip
+# end
