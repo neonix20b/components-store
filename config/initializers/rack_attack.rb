3b5ed4ef0157ffc56f892e13af4d78d971f3f783
Smolev Denis <smolev@me.com>
2024-06-08 21:33:51 +0300

    mass block

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -3,15 +3,31 @@ class Rack::Attack
   blocklist_ip("23.22.35.0/16")
   blocklist_ip("3.224.220.0/16")
 
-  throttle("requests by ip", limit: 10, period: 2.seconds) do |request|
-    request.ip unless request.path.start_with?('/assets')
+  throttle("requests by ip", limit: 10, period: 2.seconds) do |req|
+    unless req.path.start_with?('/assets')
+      Rails.logger.error("Rack::Attack Too many requests from IP: #{req.ip}")
+      req.ip
+    end
+  end
+
+  throttle('post requests', limit: 2, period: 4.seconds) do |req|
+    if req.post?
+      Rails.logger.error("Rack::Attack Too many POSTS from IP: #{req.ip}")
+      req.ip
+    end
   end
 
   blocklist('fail2ban pentesters') do |req|
-    Rack::Attack::Fail2Ban.filter("pentesters-#{req.ip}", maxretry: 3, findtime: 10.minutes, bantime: 30.minutes) do
+    Rack::Attack::Fail2Ban.filter("pentesters-#{req.ip}", maxretry: 1, findtime: 10.minutes, bantime: 60.minutes) do
       req.path.include?('/etc/passwd') ||
       req.path.include?('wp-admin') ||
-      req.path.include?('wp-login')
+      req.path.include?('wp-login') ||
+      req.path.include?('administrator') ||
+      req.path.include?('cgi-bin') ||
+      req.path.ends_with?('.php') ||
+      req.path.ends_with?('.yml') ||
+      req.path.ends_with?('.aspx') ||
+      req.path.ends_with?(".xml")
     end
   end
 end
