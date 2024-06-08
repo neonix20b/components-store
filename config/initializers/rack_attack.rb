0c8a65c7697b7a385df91bd8350a9e9520420337
Smolev Denis <smolev@me.com>
2024-06-08 20:11:03 +0300

    pentesters

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -1,7 +1,15 @@
-Rack::Attack.blocklist_ip("65.108.2.0/16")
-Rack::Attack.blocklist_ip("23.22.35.0/16")
-Rack::Attack.blocklist_ip("3.224.220.0/16")
+class Rack::Attack
+  blocklist_ip("65.108.2.0/16")
+  blocklist_ip("23.22.35.0/16")
+  blocklist_ip("3.224.220.0/16")
 
-Rack::Attack.throttle("requests by ip", limit: 5, period: 2) do |request|
-  request.ip
+  throttle("requests by ip", limit: 4, period: 2.seconds) do |request|
+    request.ip unless request.path.start_with?('/assets')
+  end
+
+  blocklist('pentesters') do |req|
+    req.path.include?('/etc/passwd') ||
+    req.path.include?('wp-admin') ||
+    req.path.include?('wp-login')
+  end
 end
