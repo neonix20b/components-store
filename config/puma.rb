04903e8f0e7f8276d60d7ebf23407cfb899f830b
Smolev Denis <smolev@me.com>
2024-06-15 20:47:58 +0300

    tune puma

--- config/puma.rb
+++ config/puma.rb
@@ -8,25 +8,33 @@
 # the maximum value specified for Puma. Default is set to 5 threads for minimum
 # and maximum; this matches the default thread size of Active Record.
 max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
-min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
+min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { 1 }
+env = ENV.fetch("RAILS_ENV") { "development" }
+
 threads min_threads_count, max_threads_count
 
 # Specifies that the worker count should equal the number of processors in production.
-if ENV["RAILS_ENV"] == "production"
+if env == "production"
   require "concurrent-ruby"
   worker_count = Integer(ENV.fetch("WEB_CONCURRENCY") { Concurrent.physical_processor_count })
-  workers worker_count if worker_count > 1
+  if worker_count > 1
+    workers worker_count
+    worker_timeout 15
+    worker_shutdown_timeout 8
+  else
+    preload_app!
+  end
+elsif env == "development"
+  # Specifies the `worker_timeout` threshold that Puma will use to wait before
+  # terminating a worker in development environments.
+  # worker_timeout 3600
 end
-
-# Specifies the `worker_timeout` threshold that Puma will use to wait before
-# terminating a worker in development environments.
-worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 port ENV.fetch("PORT") { 3000 }
 
 # Specifies the `environment` that Puma will run in.
-environment ENV.fetch("RAILS_ENV") { "development" }
+environment env
 
 # Specifies the `pidfile` that Puma will use.
 pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
