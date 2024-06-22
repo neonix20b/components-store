a890e6ac9777e30e52ba0b7efd5cbedd70978279
Smolev Denis <smolev@me.com>
2024-06-22 20:14:33 +0300

    fix

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -1,3 +1,5 @@
+require 'ox_store/product_search'
+
 # Configure Spree Preferences
 #
 # Note: Initializing preferences available within the Admin will overwrite any changes that were made through the user interface when you restart.
