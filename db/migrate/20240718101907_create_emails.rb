5021c16cecd17f5a2b06b4aa1d4e78cdf6683051
Smolev Denis <smolev@me.com>
2024-07-18 14:01:52 +0300

    read flag

--- db/migrate/20240718101907_create_emails.rb
+++ db/migrate/20240718101907_create_emails.rb
@@ -2,9 +2,10 @@ class CreateEmails < ActiveRecord::Migration[7.1]
   def change
     create_table :emails do |t|
       t.string :body
-      t.string :title
+      t.string :subject
       t.references :spree_order, null: false, foreign_key: true
       t.integer :direction, default: 0
+      t.bool :read, default: false
       t.string :from
       t.string :to
 
