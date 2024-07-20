7c2e2c9e60182854b06d98c375e11f7fba0bac9e
Smolev Denis <smolev@me.com>
2024-07-20 20:16:49 +0300

    fix blank message

--- app/controllers/telegram/webhook_controller.rb
+++ app/controllers/telegram/webhook_controller.rb
@@ -1,10 +1,12 @@
 class Telegram::WebhookController < Telegram::Bot::UpdatesController
   def message(message)
     text = message['text']
-    @bot_name ||= Telegram.bot.get_me["result"]["username"]
-    if text.include?(@bot_name)
-      text = text.delete_prefix("@").delete_prefix(@bot_name).strip
-      reply_with :message, text: text
+    unless text.blank?
+      @bot_name ||= Telegram.bot.get_me["result"]["username"]
+      if text.include?(@bot_name) || text.start_with?("@")
+        text = text.delete_prefix("@").delete_prefix(@bot_name).strip
+        reply_with :message, text: text
+      end
     end
   end
 
