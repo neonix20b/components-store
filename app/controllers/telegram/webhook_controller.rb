58d45433cfb700599fc5b658c72e0fea0b4fdf4a
Smolev Denis <smolev@me.com>
2024-07-20 20:06:05 +0300

    filter bot messages

--- app/controllers/telegram/webhook_controller.rb
+++ app/controllers/telegram/webhook_controller.rb
@@ -1,6 +1,11 @@
 class Telegram::WebhookController < Telegram::Bot::UpdatesController
   def message(message)
-    reply_with :message, text: message['text']
+    text = message['text']
+    @bot_name ||= Telegram.bot.get_me["result"]["username"]
+    if text.include?(@bot_name)
+      text = text.delete_prefix("@").delete_prefix(@bot_name).strip
+      reply_with :message, text: text
+    end
   end
 
   def start!(word = nil, *other_words)
--- lib/tasks/webhook.rake
+++ lib/tasks/webhook.rake
@@ -1,6 +1,7 @@
 desc "Set telegram webhooks"
 task webhooks: [:environment] do
     routes = Rails.application.routes.url_helpers
-    url = routes.telegram_webhook_url(host: "https://smart-components.pro")
+    url = routes.telegram_webhook_url(host: "https://#{Spree::Store.default.url}")
     Telegram.bot.set_webhook(url: url, drop_pending_updates: true)
+    
 end
