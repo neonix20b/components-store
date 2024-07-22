99b70a0cd0d37fcdda44cac0386960768d1d931a
Smolev Denis <smolev@me.com>
2024-07-22 13:18:31 +0300

    Email tabs (all, in, out)

--- app/models/email.rb
+++ app/models/email.rb
@@ -3,7 +3,7 @@ class Email < Spree::Base
   if defined?(Spree::Webhooks::HasWebhooks)
     include Spree::Webhooks::HasWebhooks
   end
-  default_scope { order(created_at: :asc) }
+  default_scope { order(created_at: :desc) }
   self.whitelisted_ransackable_attributes = %w[subject body from to direction read]
 
   belongs_to :order, class_name: 'Spree::Order', foreign_key: :spree_order_id
